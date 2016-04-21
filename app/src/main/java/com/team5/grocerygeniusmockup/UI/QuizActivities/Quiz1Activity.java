bc7e1c3615e699fc15e18f844fbb0acf3da96a0c
EoinBrennan <eoin.n.brennan@gmail.com>
2016-04-21 14:24:09 +0300

    New User pages connected to firebase.

--- app/src/main/java/com/team5/grocerygeniusmockup/UI/QuizActivities/Quiz1Activity.java
+++ app/src/main/java/com/team5/grocerygeniusmockup/UI/QuizActivities/Quiz1Activity.java
@@ -11,8 +11,11 @@ import android.widget.Toast;
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.team5.grocerygeniusmockup.Model.ListModel.List;
 import com.team5.grocerygeniusmockup.R;
+import com.team5.grocerygeniusmockup.Utilities.Constants;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -24,11 +27,13 @@ import java.util.Map;
 public class Quiz1Activity extends AppCompatActivity {
 
     EditText email_box;
+    EditText userName_ET;
     EditText password_box;
     EditText confirm_password;
     String passwordInput;
     String passwordConfirm;
     String email;
+    String username;
     Firebase ref;
     Boolean canMoveOn;
 
@@ -38,6 +43,7 @@ public class Quiz1Activity extends AppCompatActivity {
         setContentView(R.layout.activity_new_account);
 
         email_box = (EditText) findViewById(R.id.promptEmail);
+        userName_ET = (EditText) findViewById(R.id.autoCompleteTextView_NewUserName);
         password_box = (EditText) findViewById(R.id.promptPassword);
         confirm_password = (EditText) findViewById(R.id.confirmPassword);
 
@@ -50,6 +56,7 @@ public class Quiz1Activity extends AppCompatActivity {
                     public void onClick(View view) {
 
                         email = email_box.getText().toString();
+                        username = userName_ET.getText().toString();
                         passwordInput = password_box.getText().toString();
                         passwordConfirm = confirm_password.getText().toString();
 
@@ -62,19 +69,42 @@ public class Quiz1Activity extends AppCompatActivity {
                             confirm_password.setText("");
                         }else{
                            /////////////use data to create new account///////
-                            ref = new Firebase("https://logintestcs353.firebaseio.com");
+                            ref = new Firebase(Constants.FIREBASE_URL);
                             ref.createUser(email, passwordInput, new Firebase.ValueResultHandler<Map<String, Object>>() {
                                 @Override
                                 public void onSuccess(Map<String, Object> result) {
                                     Toast.makeText(getApplicationContext(), "Account successfully created",
                                             Toast.LENGTH_LONG).show();
 
+                                    String alterEmail = email.replace(".", "()");
+                                    List newList = new List(username + "'s List", username, alterEmail, result.get("uid").toString());
+
+                                    // Set up a ref in the shops section of the user's Firebase.
+                                    String FIREBASE_MY_LISTS_URL = Constants.FIREBASE_URL_LISTS + "/" + alterEmail;
+
+                                    // Initialise a Firebase object at the shops node.
+                                    Firebase myListsRef = new Firebase(FIREBASE_MY_LISTS_URL);
+
+                                    // Create a unique key for a new child and fetch it's key.
+                                    Firebase newListRef = myListsRef.push();
+                                    String newListKey = newListRef.getKey();
+
+                                    // Store the Shop object at the new child node.
+                                    newListRef.setValue(newList);
+
+                                    Firebase mySharedRef = new Firebase(Constants.FIREBASE_URL_SHARED).child(newListKey);
+                                    Map<String, Object> sharing = new HashMap<String, Object>();
+                                    sharing.put(alterEmail, true);
+                                    mySharedRef.updateChildren(sharing);
+
+
                                     //create intent
                                     Intent logInIntent = new Intent(Quiz1Activity.this, Quiz2Activity.class);
 
                                     //send data
-                                    logInIntent.putExtra("email", email);
+                                    logInIntent.putExtra("username", username);
                                     logInIntent.putExtra("password", passwordInput);
+                                    logInIntent.putExtra(Constants.KEY_LIST_ID, newListKey);
 
                                     //start activity
                                     startActivity(logInIntent);
--- app/src/main/java/com/team5/grocerygeniusmockup/UI/QuizActivities/Quiz3Activity.java
+++ app/src/main/java/com/team5/grocerygeniusmockup/UI/QuizActivities/Quiz3Activity.java
@@ -6,27 +6,35 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.firebase.client.Firebase;
+import com.team5.grocerygeniusmockup.Model.ShoppingListModel.Section;
 import com.team5.grocerygeniusmockup.R;
 import com.team5.grocerygeniusmockup.UI.LoginActivity;
+import com.team5.grocerygeniusmockup.Utilities.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Quiz3Activity extends ListActivity {
 
-    String listItem[] = {"First shop","Second shop","third shop"};
+    String listItem[] = {"Other"};
 
     ArrayAdapter<String> myAdapter;
+    TextView instructions;
     EditText et;
     ListView myList;
     String email;
+    String listkey;
+    String shopname;
+    String shopkey;
     String password;
     ArrayList<String> values;
     Firebase ref;
@@ -42,7 +50,13 @@ public class Quiz3Activity extends ListActivity {
         if (extras != null) {
             email = extras.getString("email");
             password = extras.getString("password");
+            listkey = extras.getString(Constants.KEY_LIST_ID);
+            shopname = extras.getString("shopname");
+            shopkey = extras.getString("shopkey");
         }
+
+        instructions = (TextView) findViewById(R.id.textView_newuser_addsection_inst);
+        instructions.setText("What Sections of " + shopname + " do you visit?");
 
         et = (EditText)findViewById(R.id.enterShopName);
 
@@ -61,7 +75,12 @@ public class Quiz3Activity extends ListActivity {
 
         myList = (ListView)findViewById(android.R.id.list);
         myList.setAdapter(myAdapter);
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+            }
+        });
 
         //*
         Button addShop = (Button) findViewById(R.id.button_add_new_shop);
@@ -72,10 +91,8 @@ public class Quiz3Activity extends ListActivity {
                         ArrayAdapter<String> adapter = myAdapter;
                         String device;
 
-                        List myArrayList = new ArrayList();
                         device = et.getText().toString();
-                        myArrayList.add(device);
-                        adapter.add(device);
+                        values.add(device);
                         et.setText("");
                         et.setHint("Enter another section name");
 
@@ -91,17 +108,27 @@ public class Quiz3Activity extends ListActivity {
                     public void onClick(View view) {
 
                         ////////Firebase uploads go here///////////
-                        ref = new Firebase("https://logintestcs353.firebaseio.com");
 
-                        ////////////////////////////////////////////
+                        // Set up Firebase for this shop's sections.
+                        String FIREBASE_MY_URL_SECTIONS = Constants.FIREBASE_URL + "/" + Constants.FIREBASE_NODENAME_SECTIONS + "/" + listkey + "/" + shopkey;
+                        Firebase secRef = new Firebase(FIREBASE_MY_URL_SECTIONS);
 
+                        for (int i = 1; i < values.size(); i++) {
+                            // Create a new Section object using user data and parent shop name.
+                            Section newSec = new Section(values.get(i), shopname, i);
+
+                            // Create a unique key for a new node and put the newly created section there.
+                            Firebase newSecRef = secRef.push();
+                            newSecRef.setValue(newSec);
+                        }
                         //*
                         Intent myIntent = new Intent(Quiz3Activity.this, LoginActivity.class);
 
                         //send email and password
                         myIntent.putExtra("email", email);
                         myIntent.putExtra("password", password);
-
+                        myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        myIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                         //logInIntent.putExtra("isTrue", someData);
                         startActivity(myIntent);
                         //*/
